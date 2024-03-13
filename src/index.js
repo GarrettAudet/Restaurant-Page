807b686a76803a4421221ccdfeceba4b69e6baa1
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-03-13 19:09:13 -0400

    Updated the Index Page and Added Button Functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './styles.css';
 
+// Get all buttons with the class 'menu-button'
+const buttons = document.querySelectorAll('.header-group');
+
+// Iterate over the NodeList of buttons to add the click event listener to each button
+buttons.forEach(button => {
+    
+  button.addEventListener('click', function() {
+    // Remove the 'selected' class from all buttons
+    buttons.forEach(btn => btn.classList.remove('selected'));
+    
+    // Add the 'selected' class to the clicked button
+    this.classList.add('selected');
+  });
+});
+
 console.log("Test Webpack!");
