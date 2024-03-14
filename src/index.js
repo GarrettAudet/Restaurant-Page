a22c294c7fe9d5c3c0ff8227601b8b172fe10f10
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-03-14 00:24:10 -0400

    Finished the Index Home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './styles.css';
+import Chef from './Chef.png'
 
 // Get all buttons with the class 'menu-button'
 const buttons = document.querySelectorAll('.header-group');
 
 // Iterate over the NodeList of buttons to add the click event listener to each button
 buttons.forEach(button => {
-    
+
   button.addEventListener('click', function() {
     // Remove the 'selected' class from all buttons
     buttons.forEach(btn => btn.classList.remove('selected'));
@@ -14,5 +15,7 @@ buttons.forEach(button => {
     this.classList.add('selected');
   });
 });
+
+document.getElementById('imageContainer').src = Chef;
 
 console.log("Test Webpack!");
